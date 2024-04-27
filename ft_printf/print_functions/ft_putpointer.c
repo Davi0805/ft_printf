6255c9afd83d7616b7c382c2844cc4b7f39c0fe7
Davi0805 <melocardozo.video@gmail.com>
2024-04-27 21:08:21 +0100

    .

---
+++ ft_printf/print_functions/ft_putpointer.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putpointer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davi <davi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 14:38:06 by davi              #+#    #+#             */
+/*   Updated: 2024/04/27 20:57:58 by davi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+#include <stdio.h>
+
+int	digitos(unsigned long n)
+{
+	int	nsize;
+
+	while (n != 0)
+	{
+		n /= 16;
+		nsize++;
+	}
+	return (nsize);
+}
+
+char	*inttohex(unsigned long n, int nsize)
+{
+	char	*res;
+	int		tempn;
+	int		i;
+
+	i = 0;
+	res = (char *)malloc((nsize + 3) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	while (n != 0)
+	{
+		tempn = n % 16;
+		if (tempn < 10)
+			res[i++] = tempn + '0';
+		else
+			res[i++] = tempn + 55;
+		n /= 16;
+	}
+	res[i++] = 'x';
+	res[i++] = 'O';
+	res[i] = '\0';
+	return (res);
+}
+
+int	ft_reverseputstr(char *str, int nsize)
+{
+	int	i;
+
+	i = nsize;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i--;
+	}
+	i = nsize;
+	return (i);
+}
+
+int	putpointer(void *ptr)
+{
+	char			*res;
+	unsigned long	endereco;
+	int				i;
+	int				nsize;
+
+	endereco = (unsigned long)ptr;
+	nsize = digitos(endereco);
+	res = inttohex(endereco, nsize);
+	nsize = nsize + 1;
+	ft_reverseputstr(res, nsize);
+	free(res);
+	return (nsize);
+}
+
+// int	main(void)
+// {
+// 	char	*ptr;
+
+// 	ptr = (char *)malloc(20 * sizeof(char));
+// 	putpointer(ptr);
+// }
